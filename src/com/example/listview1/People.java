675cbaa30bd46fc2b5c386ba05d922d91ddd471d
FrankLikuohao <frank.likuohao@gmail.com>
2014-10-27 20:53:01 +0800

    setData1~3

---
+++ src/com/example/listview1/People.java
@@ -0,0 +1,13 @@
+package com.example.listview1;
+
+public class People {
+	private String name;
+	private int age;
+	public People(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public String toString() {
+		return name + ":" + age;
+	}
+}
